cc367377444f519413a1977b99ac3163bda57238
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-02-24 20:54:42 +0330

    :sparkles: implement sign up page layout

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -2,9 +2,57 @@ import SignUpPage from "./SignUpPage.vue";
 import { render, screen } from "@testing-library/vue";
 import "@testing-library/jest-dom";
 
-it("has signup header", () => {
-  render(SignUpPage);
-  const header = screen.queryByRole("heading", { name: "Sign Up Page" });
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it("has signup header", () => {
+      render(SignUpPage);
+      const header = screen.queryByRole("heading", { name: "Sign Up Page" });
 
-  expect(header).toBeInTheDocument();
+      expect(header).toBeInTheDocument();
+    });
+
+    it("has username input", () => {
+      render(SignUpPage);
+      const input = screen.queryByLabelText("Username");
+      expect(input).toBeInTheDocument();
+    });
+    it("has email input", () => {
+      render(SignUpPage);
+      const input = screen.queryByTestId("emailInput");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password input", () => {
+      render(SignUpPage);
+      const input = screen.queryByTestId("passwordInput");
+      expect(input).toBeInTheDocument();
+    });
+    it("has passowrd type for passowrd input", () => {
+      render(SignUpPage);
+      const input = screen.queryByTestId("passwordInput");
+      expect(input).toHaveAttribute("type", "password");
+    });
+
+    it("has password repeat input", () => {
+      render(SignUpPage);
+      const input = screen.queryByTestId("passwordRepeatInput");
+      expect(input).toBeInTheDocument();
+    });
+    it("has passowrd type for passowrd repeat input", () => {
+      render(SignUpPage);
+      const input = screen.queryByTestId("passwordRepeatInput");
+      expect(input).toHaveAttribute("type", "password");
+    });
+
+    it("has sign up button", () => {
+      render(SignUpPage);
+      const button = screen.queryByTestId("submit");
+      expect(button).toBeInTheDocument();
+    });
+
+    it("has disabled button initially", () => {
+      render(SignUpPage);
+      const button = screen.queryByTestId("submit");
+      expect(button).toHaveAttribute("disabled");
+    });
+  });
 });
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,3 +1,28 @@
 <template>
   <h1>Sign Up Page</h1>
+
+  <form>
+    <label for="username">Username</label>
+    <input id="username" placeholder="username" />
+
+    <label for="email">E-mail</label>
+    <input id="email" placeholder="email" data-testid="emailInput" />
+
+    <label for="password">Password</label>
+    <input
+      id="password"
+      type="password"
+      placeholder="Passowrd"
+      data-testid="passwordInput"
+    />
+    <label for="password-repeat">Password Repeat</label>
+    <input
+      id="password-repeat"
+      type="password"
+      placeholder="Passowrd Repeat"
+      data-testid="passwordRepeatInput"
+    />
+
+    <button data-testid="submit" disabled>Sign Up</button>
+  </form>
 </template>
