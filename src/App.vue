42bc7ff97c8a6b19d5f8151e24f4e9c80eaea9fd
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-03-19 23:43:37 +0330

    add LanguageSelector Component in app
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <sign-up-page />
+    <language-selector />
   </div>
 </template>
 
 <script>
 import SignUpPage from "./pages/SignUpPage.vue";
+import LanguageSelector from "./components/LanguageSelector.vue";
 
 export default {
   name: "App",
   components: {
     SignUpPage,
+    LanguageSelector,
   },
 };
 </script>
