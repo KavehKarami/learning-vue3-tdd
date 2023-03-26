17185e66cd8182bc81d2502fe3f69ff782e83a46
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-03-26 21:32:25 +0330

    :recycle: refactor routing tests
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,30 +4,27 @@ import i18n from "./locales/i18n";
 import App from "./App.vue";
 
 describe("Routing", () => {
-  it("displays Home Page at /", () => {
+  it.each`
+    path         | pageTestId
+    ${"/"}       | ${"home-page"}
+    ${"/signup"} | ${"signup-page"}
+  `("displays $pageTestId at $path", ({ path, pageTestId }) => {
+    window.history.pushState({}, "", path);
     render(App, { global: { plugins: [i18n] } });
 
-    const page = screen.queryByTestId("home-page");
+    const page = screen.queryByTestId(pageTestId);
     expect(page).toBeInTheDocument();
   });
-  it("not displays Sign Up Page when at /", () => {
+
+  it.each`
+    path         | pageTestId
+    ${"/signup"} | ${"home-page"}
+    ${"/"}       | ${"signup-page"}
+  `("not displays $pageTestId when at $path", ({ path, pageTestId }) => {
+    window.history.pushState({}, "", path);
     render(App, { global: { plugins: [i18n] } });
 
-    const page = screen.queryByTestId("signup-page");
-    expect(page).not.toBeInTheDocument();
-  });
-  it("displays Sign Up Page when at /signup", () => {
-    window.history.pushState({}, "", "/signup");
-    render(App, { global: { plugins: [i18n] } });
-
-    const page = screen.queryByTestId("signup-page");
-    expect(page).toBeInTheDocument();
-  });
-  it("not displays Home Page when at /signup", () => {
-    window.history.pushState({}, "", "/signup");
-    render(App, { global: { plugins: [i18n] } });
-
-    const page = screen.queryByTestId("home-page");
+    const page = screen.queryByTestId(pageTestId);
     expect(page).not.toBeInTheDocument();
   });
 });
