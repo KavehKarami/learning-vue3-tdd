cf631aadf7b8a595cccbe0e7a3d73293456ee967
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-03-28 17:08:22 +0330

    :recycle: remove extra codes
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,11 @@
 import { render, screen } from "@testing-library/vue";
-import "@testing-library/jest-dom";
 import i18n from "./locales/i18n";
 import App from "./App.vue";
+
+const setup = (path) => {
+  window.history.pushState({}, "", path);
+  render(App, { global: { plugins: [i18n] } });
+};
 
 describe("Routing", () => {
   it.each`
@@ -11,9 +15,7 @@ describe("Routing", () => {
     ${"/login"}  | ${"login-page"}
     ${"/user/1"} | ${"user-page"}
   `("displays $pageTestId at $path", ({ path, pageTestId }) => {
-    window.history.pushState({}, "", path);
-    render(App, { global: { plugins: [i18n] } });
-
+    setup(path);
     const page = screen.queryByTestId(pageTestId);
     expect(page).toBeInTheDocument();
   });
@@ -33,8 +35,7 @@ describe("Routing", () => {
     ${"/user/1"} | ${"login-page"}
     ${"/user/1"} | ${"home-page"}
   `("not displays $pageTestId when at $path", ({ path, pageTestId }) => {
-    window.history.pushState({}, "", path);
-    render(App, { global: { plugins: [i18n] } });
+    setup(path);
 
     const page = screen.queryByTestId(pageTestId);
     expect(page).not.toBeInTheDocument();
