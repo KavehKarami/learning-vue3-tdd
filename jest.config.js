5e547e44c6ac8593077bdfdc96bc7870f3d77024
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-03-31 16:22:53 +0330

    :wrench: mock asset files
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,8 @@ module.exports = {
   },
   moduleFileExtensions: ["js", "vue", "json"],
   setupFilesAfterEnv: ["./test/setupTest.js"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/test/fileMock.js",
+  },
 };
