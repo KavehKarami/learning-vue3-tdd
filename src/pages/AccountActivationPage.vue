e124297c4cf602b29c26c57f38f2a55cc6d45280
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-04-03 14:47:51 +0330

    api call and alert box for axxount activation

--- src/pages/AccountActivationPage.vue
+++ src/pages/AccountActivationPage.vue
@@ -1,5 +1,31 @@
 <template>
   <div data-testid="activation-page">
-    <h1>Account Activation Page</h1>
+    <div
+      v-if="isSuccess"
+      class="mt-3 alert alert-success"
+      data-testid="success-message-box"
+    >
+      Account Activated Successful
+    </div>
   </div>
 </template>
+
+<script>
+import services from "../api/api.js";
+export default {
+  data() {
+    return {
+      isSuccess: false,
+    };
+  },
+
+  async mounted() {
+    try {
+      await services.activate(this.$route.params.token);
+      this.isSuccess = true;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
