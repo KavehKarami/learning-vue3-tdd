b83d10e6e2dbc2d264e054cfd4292f9251ac0305
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-04-16 10:32:55 +0330

    :tada: implement UserListItem

---
+++ src/components/UserListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    src="../assets/profile.png"
+    width="30"
+    class="rounded-circle shadow-sm"
+  />
+  {{ user.username }}
+</template>
+<script>
+export default {
+  props: {
+    user: { type: Object },
+  },
+};
+</script>
