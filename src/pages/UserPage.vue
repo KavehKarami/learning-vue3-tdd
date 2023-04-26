f95e197a01a22d899a80110d5627fe62835ff9ed
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-04-26 12:04:14 +0330

    show username in userpage

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,5 +1,25 @@
 <template>
   <div data-testid="user-page">
-    <h1>User Page</h1>
+    {{ user.username }}
   </div>
 </template>
+
+<script>
+import services from "../api/api";
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+
+  async mounted() {
+    try {
+      const { data: user } = await services.getUserById(this.$route.params.id);
+      this.user = user;
+    } catch (e) {
+      console.log(e.message);
+    }
+  },
+};
+</script>
