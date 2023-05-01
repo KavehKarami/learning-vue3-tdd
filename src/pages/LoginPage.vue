b2175671a139c48758dd5cc10cdbf84cdad22dfc
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-05-01 23:33:29 +0330

    :tada: implement login form
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,58 @@
 <template>
-  <div data-testid="login-page">
-    <h1>Login Page</h1>
+  <div
+    class="col-lg-6 offset-lg-3 col-md-8 offset-md-2"
+    data-testid="login-page"
+  >
+    <div
+      v-if="!isAccountActivation"
+      class="mt-5 card"
+      data-testid="signup-form"
+    >
+      <h1 class="card-header text-center">Login</h1>
+      <form class="card-body">
+        <base-input
+          id="email"
+          title="Email"
+          placeholder="Email"
+          data-testid="emailInput"
+          v-model="email"
+        />
+
+        <base-input
+          id="password"
+          title="Password"
+          type="password"
+          placeholder="Password"
+          data-testid="passwordInput"
+          v-model="password"
+        />
+
+        <div class="text-center">
+          <button
+            class="btn btn-outline-primary mt-3"
+            data-testid="submit"
+            disabled
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
+
+<script>
+import BaseInput from "../components/BaseInput.vue";
+export default {
+  name: "SignUpPage",
+  components: { BaseInput },
+  data() {
+    return {
+      isLoading: false,
+      isAccountActivation: false,
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
