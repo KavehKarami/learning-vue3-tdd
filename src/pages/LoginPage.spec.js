b833e7f4a5042292a99992be23a82d292ac93fd0
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-05-05 18:31:03 +0330

    :white_check_mark: test for login page translation
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

--- src/pages/LoginPage.spec.js
+++ src/pages/LoginPage.spec.js
@@ -3,13 +3,19 @@ import userEvent from "@testing-library/user-event";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 import LoginPage from "./LoginPage.vue";
+import LanguageSelector from "../components/LanguageSelector.vue";
+import i18n from "../locales/i18n";
+import en from "../locales/en.json";
+import fa from "../locales/fa.json";
 
 let requestBody,
+  acceptLanguage,
   counter = 0;
 const server = setupServer(
   rest.post("/api/1.0/auth", async (req, res, ctx) => {
     counter += 1;
     requestBody = await req.json();
+    acceptLanguage = req.headers.get("Accept-Language");
     return res(ctx.status(401), ctx.json({ message: "Incorrect credentials" }));
   })
 );
@@ -25,7 +31,7 @@ afterAll(async () => await server.close());
 
 let emailInput, passwordInput, button;
 const setup = async () => {
-  render(LoginPage);
+  render(LoginPage, { global: { plugins: [i18n] } });
 
   emailInput = screen.queryByTestId("emailInput");
   passwordInput = screen.queryByTestId("passwordInput");
@@ -130,4 +136,57 @@ describe("Login Page", () => {
       expect(failMessage).not.toBeInTheDocument();
     });
   });
-});
+
+  describe("Internationalization", () => {
+    let persianLanguage, passowrdInput, button, emailInput;
+    const setupTranslation = () => {
+      const app = {
+        name: "App",
+        components: {
+          LoginPage,
+          LanguageSelector,
+        },
+        template: `<login-page /><language-selector />`,
+      };
+      render(app, { global: { plugins: [i18n] } });
+
+      persianLanguage = screen.queryByTestId("persianLang");
+      emailInput = screen.queryByTestId("emailInput");
+      passowrdInput = screen.queryByTestId("passwordInput");
+      button = screen.queryByTestId("submit");
+    };
+
+    it("initially displays all text in english", async () => {
+      setupTranslation();
+      expect(
+        screen.queryByRole("heading", { name: en.login })
+      ).toBeInTheDocument();
+      expect(
+        screen.queryByRole("button", { name: en.login })
+      ).toBeInTheDocument();
+      expect(screen.queryByLabelText(en.email)).toBeInTheDocument();
+      expect(screen.queryByLabelText(en.password)).toBeInTheDocument();
+    });
+    it("displays all text in persian after changing language", async () => {
+      setupTranslation();
+      await userEvent.click(persianLanguage);
+      expect(
+        screen.queryByRole("heading", { name: fa.login })
+      ).toBeInTheDocument();
+      expect(
+        screen.queryByRole("button", { name: fa.login })
+      ).toBeInTheDocument();
+      expect(screen.queryByLabelText(fa.email)).toBeInTheDocument();
+      expect(screen.queryByLabelText(fa.password)).toBeInTheDocument();
+    });
+
+    xit("sends accept-language header as fa in login request", async () => {
+      setupTranslation();
+      await userEvent.click(persianLanguage);
+      await userEvent.type(emailInput, "a");
+      await userEvent.type(passowrdInput, "asd");
+      await userEvent.click(button);
+      expect(acceptLanguage).toBe("fa");
+    });
+  });
+}); 
