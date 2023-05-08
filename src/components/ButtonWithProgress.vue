b336e0161aa8d20abd51c26104e134f8ad122a6a
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-05-08 21:59:35 +0330

    :tada: implement ButtonWithProgress component
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

---
+++ src/components/ButtonWithProgress.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    class="btn btn-outline-primary mt-3"
+    data-testid="submit"
+    :disabled="disabled"
+    @click.prevent="onClick"
+  >
+    <base-spinner v-if="isLoading" />
+    <slot>Click</slot>
+  </button>
+</template>
+
+<script>
+import BaseSpinner from "./BaseSpinner.vue";
+export default {
+  components: { BaseSpinner },
+  props: {
+    disabled: { type: Boolean },
+    isLoading: { type: Boolean },
+    onClick: { type: Function },
+  },
+};
+</script>
