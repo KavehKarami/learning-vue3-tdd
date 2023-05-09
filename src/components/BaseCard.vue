98323c53cf4a837cab215c42a91e762738e13600
Kaveh Karami <kaveh.karami.kk@gmail.com>
2023-05-09 11:04:20 +0330

    :tada: implement base-card component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card mt-5">
+    <div class="card-header text-center">
+      <slot name="header"></slot>
+    </div>
+    <div v-if="$slots.body" class="card-body">
+      <slot name="body"></slot>
+    </div>
+    <slot></slot>
+
+    <div v-if="$slots.footer" class="card-footer text-center">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
