3130adcccbd0a372b26f6255959a3da7cd7d3f47
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-05-12 17:38:02 +0330

    :tada: NavBar Component

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,5 @@
 <template>
-  <div class="shadow-sm bg-light">
-    <nav class="navbar navbar-expand">
-      <div class="container">
-        <router-link
-          class="navbar-brand"
-          to="/"
-          data-testid="homepage-nav-link"
-        >
-          <img
-            src="./assets/hoaxify.png"
-            width="60"
-            alt="Hoaxify Logo"
-            data-testid="brand-logo"
-          />
-
-          Hoaxify
-        </router-link>
-
-        <ul class="navbar-nav ms-auto">
-          <li v-if="!$store.state.isLoggedIn">
-            <router-link
-              class="nav-link"
-              to="/signup"
-              data-testid="signup-nav-link"
-            >
-              {{ $t("sign_up") }}
-            </router-link>
-          </li>
-          <li v-if="!$store.state.isLoggedIn">
-            <router-link
-              class="nav-link"
-              to="/login"
-              data-testid="login-nav-link"
-            >
-              {{ $t("login") }}
-            </router-link>
-          </li>
-          <li v-if="$store.state.isLoggedIn">
-            <router-link
-              class="nav-link"
-              :to="'/user/' + $store.state.id"
-              data-testid="profile-nav-link"
-            >
-              My Profile
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
+  <nav-bar />
 
   <div class="container mt-3 mt-md-4">
     <router-view />
@@ -58,11 +9,13 @@
 
 <script>
 import LanguageSelector from "./components/LanguageSelector.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "App",
   components: {
     LanguageSelector,
+    NavBar,
   },
 };
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="shadow-sm bg-light">
+    <nav class="navbar navbar-expand">
+      <div class="container">
+        <router-link
+          class="navbar-brand"
+          to="/"
+          data-testid="homepage-nav-link"
+        >
+          <img
+            src="../assets/hoaxify.png"
+            width="60"
+            alt="Hoaxify Logo"
+            data-testid="brand-logo"
+          />
+
+          Hoaxify
+        </router-link>
+
+        <ul class="navbar-nav ms-auto">
+          <li v-if="!$store.state.isLoggedIn">
+            <router-link
+              class="nav-link"
+              to="/signup"
+              data-testid="signup-nav-link"
+            >
+              {{ $t("sign_up") }}
+            </router-link>
+          </li>
+          <li v-if="!$store.state.isLoggedIn">
+            <router-link
+              class="nav-link"
+              to="/login"
+              data-testid="login-nav-link"
+            >
+              {{ $t("login") }}
+            </router-link>
+          </li>
+          <li v-if="$store.state.isLoggedIn">
+            <router-link
+              class="nav-link"
+              :to="'/user/' + $store.state.id"
+              data-testid="profile-nav-link"
+            >
+              My Profile
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
